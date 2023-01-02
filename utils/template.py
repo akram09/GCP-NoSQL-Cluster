4de52d6219457f5354be35aafc867941549b296d
m0kr4n3 <jm_abdelmalek@esi.dz>
2023-01-02 08:43:27 -0500

    Reorganize project structure

---
+++ utils/template.py
@@ -0,0 +1,69 @@
+from google.cloud import compute_v1
+from utils.core import wait_for_extended_operation
+
+
+def create_template(project_id: str, template_name: str) -> compute_v1.InstanceTemplate:
+    """
+    Create a new instance template with the provided name and a specific
+    instance configuration.
+    Args:
+        project_id: project ID or project number of the Cloud project you use.
+        template_name: name of the new template to create.
+    Returns:
+        InstanceTemplate object that represents the new instance template.
+    """
+    # The template describes the size and source image of the boot disk
+    # to attach to the instance.
+    disk = compute_v1.AttachedDisk()
+    initialize_params = compute_v1.AttachedDiskInitializeParams()
+    initialize_params.source_image = (
+        "projects/debian-cloud/global/images/family/debian-11"
+    )
+    initialize_params.disk_size_gb = 250
+    disk.initialize_params = initialize_params
+    disk.auto_delete = True
+    disk.boot = True
+
+    # The template connects the instance to the `default` network,
+    # without specifying a subnetwork.
+    network_interface = compute_v1.NetworkInterface()
+    network_interface.name = "global/networks/default"
+
+    # The template lets the instance use an external IP address.
+    access_config = compute_v1.AccessConfig()
+    access_config.name = "External NAT"
+    access_config.type_ = "ONE_TO_ONE_NAT"
+    access_config.network_tier = "PREMIUM"
+    network_interface.access_configs = [access_config]
+
+    template = compute_v1.InstanceTemplate()
+    template.name = template_name
+    template.properties.disks = [disk]
+    template.properties.machine_type = "e2-standard-4"
+    template.properties.network_interfaces = [network_interface]
+
+    template_client = compute_v1.InstanceTemplatesClient()
+    operation = template_client.insert(
+        project=project_id, instance_template_resource=template
+    )
+
+    wait_for_extended_operation(operation, "instance template creation")
+
+    return template_client.get(project=project_id, instance_template=template_name)
+
+
+def get_instance_template(
+    project_id: str, template_name: str
+) -> compute_v1.InstanceTemplate:
+    """
+    Retrieve an instance template, which you can use to create virtual machine
+    (VM) instances and managed instance groups (MIGs).
+    Args:
+        project_id: project ID or project number of the Cloud project you use.
+        template_name: name of the template to retrieve.
+    Returns:
+        InstanceTemplate object that represents the retrieved template.
+    """
+    template_client = compute_v1.InstanceTemplatesClient()
+    return template_client.get(project=project_id, instance_template=template_name)
+
---
+++ utils/vm_instance.py
@@ -0,0 +1,190 @@
+
+from typing import Iterable
+from google.cloud import compute_v1
+import re
+from typing import List
+import warnings
+from utils.core import wait_for_extended_operation
+
+def create_instance(
+    project_id: str,
+    zone: str,
+    instance_name: str,
+    disks: List[compute_v1.AttachedDisk],
+    machine_type: str = "n1-standard-1",
+    network_link: str = "global/networks/default",
+    subnetwork_link: str = None,
+    internal_ip: str = None,
+    external_access: bool = False,
+    external_ipv4: str = None,
+    accelerators: List[compute_v1.AcceleratorConfig] = None,
+    preemptible: bool = False,
+    spot: bool = False,
+    instance_termination_action: str = "STOP",
+    custom_hostname: str = None,
+    delete_protection: bool = False,
+) -> compute_v1.Instance:
+    """
+    Send an instance creation request to the Compute Engine API and wait for it to complete.
+    Args:
+        project_id: project ID or project number of the Cloud project you want to use.
+        zone: name of the zone to create the instance in. For example: "us-west3-b"
+        instance_name: name of the new virtual machine (VM) instance.
+        disks: a list of compute_v1.AttachedDisk objects describing the disks
+            you want to attach to your new instance.
+        machine_type: machine type of the VM being created. This value uses the
+            following format: "zones/{zone}/machineTypes/{type_name}".
+            For example: "zones/europe-west3-c/machineTypes/f1-micro"
+        network_link: name of the network you want the new instance to use.
+            For example: "global/networks/default" represents the network
+            named "default", which is created automatically for each project.
+        subnetwork_link: name of the subnetwork you want the new instance to use.
+            This value uses the following format:
+            "regions/{region}/subnetworks/{subnetwork_name}"
+        internal_ip: internal IP address you want to assign to the new instance.
+            By default, a free address from the pool of available internal IP addresses of
+            used subnet will be used.
+        external_access: boolean flag indicating if the instance should have an external IPv4
+            address assigned.
+        external_ipv4: external IPv4 address to be assigned to this instance. If you specify
+            an external IP address, it must live in the same region as the zone of the instance.
+            This setting requires `external_access` to be set to True to work.
+        accelerators: a list of AcceleratorConfig objects describing the accelerators that will
+            be attached to the new instance.
+        preemptible: boolean value indicating if the new instance should be preemptible
+            or not. Preemptible VMs have been deprecated and you should now use Spot VMs.
+        spot: boolean value indicating if the new instance should be a Spot VM or not.
+        instance_termination_action: What action should be taken once a Spot VM is terminated.
+            Possible values: "STOP", "DELETE"
+        custom_hostname: Custom hostname of the new VM instance.
+            Custom hostnames must conform to RFC 1035 requirements for valid hostnames.
+        delete_protection: boolean value indicating if the new virtual machine should be
+            protected against deletion or not.
+    Returns:
+        Instance object.
+    """
+    instance_client = compute_v1.InstancesClient()
+
+    # Use the network interface provided in the network_link argument.
+    network_interface = compute_v1.NetworkInterface()
+    network_interface.name = network_link
+    if subnetwork_link:
+        network_interface.subnetwork = subnetwork_link
+
+    if internal_ip:
+        network_interface.network_i_p = internal_ip
+
+    if external_access:
+        access = compute_v1.AccessConfig()
+        access.type_ = compute_v1.AccessConfig.Type.ONE_TO_ONE_NAT.name
+        access.name = "External NAT"
+        access.network_tier = access.NetworkTier.PREMIUM.name
+        if external_ipv4:
+            access.nat_i_p = external_ipv4
+        network_interface.access_configs = [access]
+
+    # Collect information into the Instance object.
+    instance = compute_v1.Instance()
+    instance.network_interfaces = [network_interface]
+    instance.name = instance_name
+    instance.disks = disks
+    if re.match(r"^zones/[a-z\d\-]+/machineTypes/[a-z\d\-]+$", machine_type):
+        instance.machine_type = machine_type
+    else:
+        instance.machine_type = f"zones/{zone}/machineTypes/{machine_type}"
+
+    if accelerators:
+        instance.guest_accelerators = accelerators
+
+    if preemptible:
+        # Set the preemptible setting
+        warnings.warn(
+            "Preemptible VMs are being replaced by Spot VMs.", DeprecationWarning
+        )
+        instance.scheduling = compute_v1.Scheduling()
+        instance.scheduling.preemptible = True
+
+    if spot:
+        # Set the Spot VM setting
+        instance.scheduling = compute_v1.Scheduling()
+        instance.scheduling.provisioning_model = (
+            compute_v1.Scheduling.ProvisioningModel.SPOT.name
+        )
+        instance.scheduling.instance_termination_action = instance_termination_action
+
+    if custom_hostname is not None:
+        # Set the custom hostname for the instance
+        instance.hostname = custom_hostname
+
+    if delete_protection:
+        # Set the delete protection bit
+        instance.deletion_protection = True
+
+    # Prepare the request to insert an instance.
+    request = compute_v1.InsertInstanceRequest()
+    request.zone = zone
+    request.project = project_id
+    request.instance_resource = instance
+
+    # Wait for the create operation to complete.
+    print(f"Creating the {instance_name} instance in {zone}...")
+
+    operation = instance_client.insert(request=request)
+
+    wait_for_extended_operation(operation, "instance creation")
+
+    print(f"Instance {instance_name} created.")
+    return instance_client.get(project=project_id, zone=zone, instance=instance_name)
+
+
+def create_instance_from_template(
+    project_id: str, zone: str, instance_name: str, instance_template_url: str
+) -> compute_v1.Instance:
+    """
+    Creates a Compute Engine VM instance from an instance template.
+    Args:
+        project_id: ID or number of the project you want to use.
+        zone: Name of the zone you want to check, for example: us-west3-b
+        instance_name: Name of the new instance.
+        instance_template_url: URL of the instance template used for creating the new instance.
+            It can be a full or partial URL.
+            Examples:
+            - https://www.googleapis.com/compute/v1/projects/project/global/instanceTemplates/example-instance-template
+            - projects/project/global/instanceTemplates/example-instance-template
+            - global/instanceTemplates/example-instance-template
+    Returns:
+        Instance object.
+    """
+    instance_client = compute_v1.InstancesClient()
+
+    instance_insert_request = compute_v1.InsertInstanceRequest()
+    instance_insert_request.project = project_id
+    instance_insert_request.zone = zone
+    instance_insert_request.source_instance_template = instance_template_url
+    instance_insert_request.instance_resource.name = instance_name
+
+    operation = instance_client.insert(instance_insert_request)
+    wait_for_extended_operation(operation, "instance creation")
+
+    return instance_client.get(project=project_id, zone=zone, instance=instance_name)
+
+
+def list_instances(project_id: str, zone: str) -> Iterable[compute_v1.Instance]:
+    """
+    List all instances in the given zone in the specified project.
+
+    Args:
+        project_id: project ID or project number of the Cloud project you want to use.
+        zone: name of the zone you want to use. For example: “us-west3-b”
+    Returns:
+        An iterable collection of Instance objects.
+    """
+    instance_client = compute_v1.InstancesClient()
+    instance_list = instance_client.list(project=project_id, zone=zone)
+
+    print(f"Instances found in zone {zone}:")
+    for instance in instance_list:
+        print(f" - {instance.name} ({instance.machine_type})")
+
+    return instance_list
+
