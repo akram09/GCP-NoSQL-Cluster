176cf7cf8d67f16cef976033baad5326d7946b22
m0kr4n3 <jm_abdelmalek@esi.dz>
2023-01-02 05:47:01 -0500

    Add list_vm_instances function

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from google.cloud import storage
-from .config import PROJECT_ID
+from config import PROJECT_ID
+from typing import Iterable
+from google.cloud import compute_v1
 
+def list_instances(project_id: str, zone: str) -> Iterable[compute_v1.Instance]:
+    """
+    List all instances in the given zone in the specified project.
+
+    Args:
+        project_id: project ID or project number of the Cloud project you want to use.
+        zone: name of the zone you want to use. For example: “us-west3-b”
+    Returns:
+        An iterable collection of Instance objects.
+    """
+    instance_client = compute_v1.InstancesClient()
+    instance_list = instance_client.list(project=project_id, zone=zone)
+
+    print(f"Instances found in zone {zone}:")
+    for instance in instance_list:
+        print(f" - {instance.name} ({instance.machine_type})")
+
+    return instance_list
 
 def list_storage_buckets(project_id="your-google-cloud-project-id"):
     storage_client = storage.Client(project=project_id)
@@ -11,4 +31,4 @@ def list_storage_buckets(project_id="your-google-cloud-project-id"):
     print("Listed all storage buckets.")
 
 if __name__ == "__main__":
-    list_storage_buckets(project_id=PROJECT_ID)
+    list_instances(PROJECT_ID, "us-west3-b")
