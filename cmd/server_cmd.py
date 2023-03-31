0094c03db5a7ff3c714e0c24de50d3115b1a13d3
akram09 <a.boutouchent@esi-sba.dz>
2023-03-31 13:55:45 +0200

    Adding auth routes

--- cmd/server_cmd.py
+++ cmd/server_cmd.py
@@ -7,12 +7,8 @@ def start_server(args):
     logger.info("Welcome to the server sub command ")
     # setting flask server configuration 
     logger.info("Setting flask server configuration ...")
-    app_config = {
-        'ENV': 'development',
-        'SECRET_KEY': base64.b64encode(os.urandom(24)).decode('utf-8')
-    }
     # create and configure the app
-    app = create_app(app_config)
+    app = create_app()
     # start the server
     app.run(host=args.host, port=args.port, debug=args.debug)
     
